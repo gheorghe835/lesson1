b35cdf23b70445d37690c27d4eb1d6cec82b2e8c
gbati <gheorghe835@gmail.com>
2024-03-17 21:26:39 +0200

    added lesson:task_003

---
+++ lesson1/task_003/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("  Enter number N :: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int valHundred = N / 100;
+int valUnit = N % 10;
+int valTen = N / 10 % 10;
+int summ = valHundred + valUnit;
+
+Console.WriteLine($" value hundred :: {valHundred}");
+Console.WriteLine($" value ten :: {valTen}");
+Console.WriteLine($" value unit :: {valUnit}");
+Console.WriteLine($" Summ of numbers :: {summ}");
