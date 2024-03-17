87ac497bc0712711ac27512e7d6529544ae017e9
gbati <gheorghe835@gmail.com>
2024-03-17 13:15:27 +0200

    added some values lesson1:task_01

--- lesson1/task_01/Program.cs
+++ lesson1/task_01/Program.cs
@@ -1,2 +1,12 @@
-﻿Console.WriteLine(" HELLO WORLD !!!! ");
-Console.WriteLine(" SALUT LUME  !!!! ");
+﻿//Console.WriteLine(" HELLO WORLD !!!! ");
+//Console.WriteLine(" SALUT LUME  !!!! ");
+
+int value = 100;
+Console.WriteLine("  Value = " + value);
+
+double d = 5.55;
+char ch = 'W';
+bool b = true;
+Console.WriteLine("  Double = " + d);
+Console.WriteLine("  Char = " + ch);
+Console.WriteLine("  Boolean = " + b);
