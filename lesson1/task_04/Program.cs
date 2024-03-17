da11d79dd140ae7912b8626149e8ff9cae8b9eb2
gbati <gheorghe835@gmail.com>
2024-03-17 14:24:50 +0200

    added lesson1:task_04

---
+++ lesson1/task_04/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 6;
+int b = 75;
+int c = 9;
+int d = 4;
+int e = 12;
+
+int max = a;
+
+if (b > max) { max = b; }
+if (c > max) { max = c; }
+if (d > max) { max = d; }
+if (e > max) { max = e; }
+
+Console.WriteLine($"  Maximum is :: {max}");
