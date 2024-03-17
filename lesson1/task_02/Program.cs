f92fcdb6af44590b6291f05511b4dce523eaa0b0
gbati <gheorghe835@gmail.com>
2024-03-17 13:32:52 +0200

    added values lesson1: task_02

---
+++ lesson1/task_02/Program.cs
@@ -0,0 +1,15 @@
+﻿int i1 = 5;
+int i2 = 6;
+int summ = i1 + i2;
+
+Console.WriteLine($" Summa :: {i1} + {i2} = {summ}");
+
+long l1 = 66;
+long summ2 = l1 + i1;
+
+Console.WriteLine($" Summa2 :: {l1} + {i1} = {summ2}");
+
+double d1 = 5.5;
+double prod1 = d1 * l1;
+
+Console.WriteLine($" Produce2 :: {d1} + {l1} = {prod1}");
