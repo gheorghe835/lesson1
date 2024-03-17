738183c262666a10599314af2a47fdfd3aa40cf2
gbati <gheorghe835@gmail.com>
2024-03-17 20:54:47 +0200

    added lesson1:task_002

---
+++ lesson1/task_002/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Task :: -N to N ");
+Console.WriteLine("  Enter number N :: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int notN = -N;
+
+while(notN <= N)
+{
+    Console.Write($" {notN} ");
+    notN++;
+}
