364893361eec11ff64ea22e3de11e1e6ccfa1772
gbati <gheorghe835@gmail.com>
2024-03-17 20:39:57 +0200

    added lesson1:task_001

---
+++ lesson1/task_001/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("  Enter first number ::");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("  Enter second number ::");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 == n2 * n2)
+{
+    Console.WriteLine($" Square of the number :: {n2} equals :: {n1}");
+}
+else
+    Console.WriteLine(" Square of the number :: {0} not equals :: {1}",
+                        n2, n1);
