5813f7eb52068b6b92eac176e763dd390ab21b27
gbati <gheorghe835@gmail.com>
2024-03-24 12:36:34 +0200

    added lesson:task_12

---
+++ lesson1/task_12/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("  Enter number N :: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[]array = new int[N];
+for(int i=0; i<N; i++)
+{
+    array[i] = i ;
+    Console.Write(array[i] + " ");
+}
