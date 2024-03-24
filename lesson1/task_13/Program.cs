54cf72f96d115c2a4a95602f0142c241b27f15d2
gbati <gheorghe835@gmail.com>
2024-03-24 15:53:23 +0200

    added lesson2:task_13

---
+++ lesson1/task_13/Program.cs
@@ -0,0 +1,43 @@
+﻿
+int n = 10;
+int max = 0;
+ int num3 = new Random().Next(10, 100);
+Console.WriteLine($"num3 :: {num3}");
+int[] array = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    array[i] = new Random().Next(10, 100);
+}
+
+for (int i = 0; i < n; i++)
+{
+     Console.Write(array[i] + " ");
+}
+Console.WriteLine("\n ----------------");
+
+for (int i = 0; i < n; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+Console.WriteLine("\n ----------------");
+for (int i = 0; i < n; i++)
+{
+    if (array[i] % 2 != 0)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+for (int i = 0; i < n; i++)
+{
+    if (max < array[i] )
+    {
+        max = array[i];
+         
+    }
+}
+Console.Write($"\n Maximum :: {max}");
+
